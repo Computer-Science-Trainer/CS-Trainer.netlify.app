f85a56b88640a392808ce2cc4991a3c0d13b76cd
ariollex <ariollex@gmail.com>
2025-03-25 00:12:23 +0300

    navbar: menu: fixup links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -203,7 +203,7 @@ export const Navbar = () => {
                       ? "danger"
                       : "foreground"
                 }
-                href="#"
+                href={item.href}
                 size="lg"
               >
                 {t(item.label)}
