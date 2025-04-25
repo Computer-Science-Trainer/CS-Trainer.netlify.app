de2f3b7d7408299b9d2ab1081d77574dbfc61a07
ariollex <ariollex@gmail.com>
2025-04-25 17:54:50 +0300

    navbar: Drop search

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,9 +9,7 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
@@ -136,27 +134,6 @@ export const Navbar = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const t = useTranslations();
 
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder={t("nav.search_placeholder")}
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -166,7 +143,7 @@ export const Navbar = () => {
             <p className="font-bold text-inherit">CS Trainer</p>
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className="hidden sm:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
@@ -198,7 +175,6 @@ export const Navbar = () => {
           </Link>
           <SettingsDropdown />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
         {user ? (
           <NextLink
             className="flex items-center gap-2 hover:bg-default-100 rounded-xl p-2 transition-colors duration-200 ease-in-out cursor-pointer"
@@ -225,7 +201,6 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
