00ad6f5c2e39f98176b14010839b5a4624e50939
ariollex <ariollex@gmail.com>
2025-05-10 15:42:50 +0300

    about: Redesign

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { title } from "@/components/primitives";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { InformationCircleIcon, BookOpenIcon, Mail02Icon } from "@hugeicons/core-free-icons";
+import { InformationCircleIcon, BookOpenIcon, Mail02Icon, ChartUpIcon, Edit02Icon } from "@hugeicons/core-free-icons";
 import { useTranslations } from "next-intl";
+import { Card } from "@heroui/react";
 
 export default function AboutPage() {
   const t = useTranslations();
@@ -11,23 +14,42 @@ export default function AboutPage() {
       <p className="mt-4 text-lg leading-7">
         {t("about.description")}
       </p>
-      <ul className="list-none mt-4 text-base space-y-1">
-        <li className="flex items-center gap-2 text-left">
-          <HugeiconsIcon icon={BookOpenIcon} className="text-xl text-primary" />
-          {t("about.features.tests")}
-        </li>
-        <li className="flex items-center gap-2 text-left">
-          <HugeiconsIcon icon={InformationCircleIcon} className="text-xl text-primary" />
-          {t("about.features.feedback")}
-        </li>
-      </ul>
-      <div className="mt-8">
-        <p className="font-semibold text-lg">{t("about.contacts")}:</p>
-        <p className="flex items-center gap-2 mt-2">
-          <HugeiconsIcon icon={Mail02Icon} className="text-xl text-primary" />
-          info@cs-trainer.netlify.app
-        </p>
+      <h2 className="mt-8 text-2xl font-semibold text-center md:text-left">Features</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
+        <Card className="flex w-full items-start gap-4 dark:bg-zinc-800 p-6 border-3 dark:border-zinc-800 rounded-3xl dark:bg-zinc-900" shadow="none">
+          <p className="flex items-center gap-2 text-base font-medium">
+            <HugeiconsIcon icon={BookOpenIcon} className="text-2xl text-primary" />
+            {t("about.features.tests")}
+          </p>
+        </Card>
+        <Card className="flex w-full items-start gap-4 dark:bg-zinc-800 p-6 border-3 dark:border-zinc-800 rounded-3xl dark:bg-zinc-900" shadow="none">
+          <p className="flex items-center gap-2 text-base font-medium">
+            <HugeiconsIcon icon={InformationCircleIcon} className="text-2xl text-primary" />
+            {t("about.features.feedback")}
+          </p>
+        </Card>
+        <Card className="flex w-full items-start gap-4 dark:bg-zinc-800 p-6 border-3 dark:border-zinc-800 rounded-3xl dark:bg-zinc-900" shadow="none">
+          <p className="flex items-center gap-2 text-base font-medium">
+            <HugeiconsIcon icon={ChartUpIcon} className="text-2xl text-primary" />
+            {t("about.features.statistics")}
+          </p>
+        </Card>
+        <Card className="flex w-full items-start gap-4 dark:bg-zinc-800 p-6 border-3 dark:border-zinc-800 rounded-3xl dark:bg-zinc-900" shadow="none">
+          <p className="flex items-center gap-2 text-base font-medium">
+          <HugeiconsIcon icon={Edit02Icon} className="text-2xl text-primary" />
+            {t("about.features.suggestQuestion")}
+          </p>
+        </Card>
       </div>
+      <h2 className="mt-8 text-2xl font-semibold text-center md:text-left">{t("about.contacts")}:</h2>
+      <Card className="mt-8 dark:bg-zinc-800 p-6 border-3 dark:border-zinc-800 rounded-3xl dark:bg-zinc-900" shadow="none">
+        <p className="flex items-center gap-2">
+          <HugeiconsIcon icon={Mail02Icon} className="text-xl text-primary" />
+          <a href="mailto:cs-trainer@list.ru" className="text-base hover:underline">
+            cs-trainer@list.ru
+          </a>
+        </p>
+      </Card>
     </div>
   );
 }
